da17362564c8c6c269a391ec589bb05e91236e34
pablomorro <al361654@uji.es>
2019-03-30 22:47:32 +0100

    Merge branch 'master' of https://github.com/CesarDiazDelgado/GasPrices
    # Conflicts:
    #	app/src/main/java/com/example/rarct/gasprices/MainActivity.java

--- app/src/main/java/com/example/rarct/gasprices/ShowActivity.java
+++ app/src/main/java/com/example/rarct/gasprices/ShowActivity.java
@@ -1,22 +1,16 @@
 package com.example.rarct.gasprices;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.os.Parcel;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class ShowActivity extends Activity {
 
@@ -26,9 +20,9 @@ public class ShowActivity extends Activity {
     public static ProgressBar progressBar;
     private static String[] aux;
     private static String[] auxMaps;
+    private static String[] direction;
 
     private static Context context;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +31,7 @@ public class ShowActivity extends Activity {
         // recovering the instance state
         if (savedInstanceState != null) {
             showPresenter = new ShowPresenter(this, null);
-            //mainPresenter.setState(savedInstanceState);
         }else {
-            showPresenter = new ShowPresenter(this, MainModel.getInstance(getBaseContext()));
-
             stationPrice = new StationPrice(MainActivity.url, this) {
                 @Override
                 public int describeContents() {
@@ -52,23 +43,21 @@ public class ShowActivity extends Activity {
 
                 }
             };
-
+            showPresenter = new ShowPresenter(this, stationPrice);
         }
         context = this;
         setContentView(R.layout.show_gas_price);
 
         listView = findViewById(R.id.listView);
         progressBar = findViewById(R.id.progressBar);
-        stationPrice.Final();
-        //Maps();
+        showPresenter.StartQuery();
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                double n1 = stationPrice.parseDouble(auxMaps[position]);
-                double n2= stationPrice.parseDouble(auxMaps[position + 1]);
-                String s = n1 + "," + n2;
-                String name = aux[position];
+                double n1 = showPresenter.parseDouble(auxMaps[position]);
+                double n2= showPresenter.parseDouble(auxMaps[position + 1]);
+                String name = direction[position];
                 Maps(n1 +"", n2 +"", name);
             }
         });
@@ -77,9 +66,11 @@ public class ShowActivity extends Activity {
 
     public static void FillListView(String[] s) {
         aux = new String[s.length/5];
+        direction = new String[s.length/5];
         auxMaps = new String[aux.length*2];
         for (int i = 0, j = 0, k = 0; i < aux.length; i++, j+=5, k+=2) {
             aux[i] = s[j] + "  " + s[j +1];
+            direction[i] = s[j +1];
             auxMaps[k] = s[j +3];
             auxMaps[k +1] = s[j +4];
         }
@@ -90,15 +81,14 @@ public class ShowActivity extends Activity {
 
     public void Maps(String s1, String s2, String name) {
         // Create a Uri from an intent string. Use the result to create an Intent.
-        //Uri gmmIntentUri = Uri.parse("google.streetview:cbll=" + s);
-        Uri gmmIntentUri = Uri.parse("geo" + s1 + "," + s2 + "?q=" + Uri.encode("AVENIDA PERIODISTA AZZATI"));
+        Uri gmmIntentUri = Uri.parse("geo:" + s1 + "," + s2 + "?q=" + Uri.encode(name));
 
-// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-// Make the Intent explicit by setting the Google Maps package
+        // Make the Intent explicit by setting the Google Maps package
         mapIntent.setPackage("com.google.android.apps.maps");
 
-// Attempt to start an activity that can handle the Intent
+        // Attempt to start an activity that can handle the Intent
         startActivity(mapIntent);
 
     }
